08c70ea7418a36a915a57923afe09950358f172f
rogermawer <rogermawer@gmail.com>
2018-09-21 18:30:14 -0700

    change

--- www/insert.php
+++ www/insert.php
@@ -1,22 +1,27 @@
 <?php
 
 
- include "constants.php";
-
+ require "constants.php";
 
 
  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-     $rname=$_POST['receiver'];
-     $new_points=(int)($_POST['points']);
+     $rname=test_input($_POST['receiver']);
      $comment=test_input($_POST['comment']);
+     $new_points=(int)($_POST['points']);
      $giver=test_input($_POST['giver']);
     
-      $users_points = "SELECT points FROM members WHERE user_name='$rname'";
-      $users_points_result = mysqli_query($con, $users_points);
-      $row = mysqli_fetch_array($users_points_result);
-      $old_points = $row['points'];
+    function newUserPoints($user){
+        include "constants.php";
+        $users_points = "SELECT points FROM members WHERE user_name='$user'";
+        $users_points_result = mysqli_query($con, $users_points);
+        $row = mysqli_fetch_array($users_points_result);
+        $old_points = $row['points'];
+        $new_points=(int)($_POST['points']);
+        return $new_points + $old_points;
+    }
+
+     $updated_points = newUserPoints($rname);
      
-     $updated_points = $new_points + $old_points;
      $q=mysqli_query($con,"UPDATE `members` SET points='$updated_points' WHERE user_name='$rname'");
      if($q){
       echo "success";
@@ -46,6 +51,7 @@ function archive($givername, $rname, $new_points){
         }
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $receivername = $rname;
+            $new_points=(int)($_POST['points']);
             date_default_timezone_set('America/Los_Angeles');
             $comment = test_input($_POST["comment"]);
             $timestamp = date('Y.m.d.h.i.s');
